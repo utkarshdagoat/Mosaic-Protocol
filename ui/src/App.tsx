e5c193fa80d8ade2d16010b27817694c7d4700aa
Garv Makkar <garv.codes@gmail.com>
2024-06-21 02:08:10 +0530

    setup theme + fonts

---
+++ ui/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='font-mosaic text-primary'>Mosaic <span className='font-sans text-muted-foreground'>Protocol</span></p>
+    </>
+  )
+}
+
+export default App
