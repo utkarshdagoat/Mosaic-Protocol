32affc6a08ba67cc82c81239ae6ba34bf7577092
utkarshdagoat <utkarsh382004@gmail.com>
2024-06-21 04:03:18 +0530

    Intial archway code

---
+++ core/contracts/mosaic-protcol/examples/schema.rs
@@ -0,0 +1,26 @@
+use std::fs::create_dir_all;
+use std::path::PathBuf;
+
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+
+use cw20::{
+    AllAccountsResponse, AllAllowancesResponse, AllowanceResponse, BalanceResponse,
+    TokenInfoResponse,
+};
+use mosaic_protcol::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    let mut out_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(AllowanceResponse), &out_dir);
+    export_schema(&schema_for!(BalanceResponse), &out_dir);
+    export_schema(&schema_for!(TokenInfoResponse), &out_dir);
+    export_schema(&schema_for!(AllAllowancesResponse), &out_dir);
+    export_schema(&schema_for!(AllAccountsResponse), &out_dir);
+}
